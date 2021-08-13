93db5fb591cb19c21f71311cd6170cd860627219
adja <adja.kane@cabify.com>
2021-08-13 22:19:08 +0200

    add artists and albums endpoints

--- stylesage_api/stylesage_api/urls.py
+++ stylesage_api/stylesage_api/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from music.views import get_artist, get_albums_with_songs
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('artists/', get_artist),
+    path('albums/', get_albums_with_songs),
+
 ]
