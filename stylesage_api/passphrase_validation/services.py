936e04cb56dbb7699486228e8fbbbf8df7edcff3
adja <adja.kane@cabify.com>
2021-08-14 12:57:19 +0200

    finish implementing passphrase endpoint

--- stylesage_api/passphrase_validation/services.py
+++ stylesage_api/passphrase_validation/services.py
@@ -1,27 +1,48 @@
-def fetch_count_validation(words):
-    unique_word = []
-    word_list = words.split(' ')
-    for word in word_list:
-        if word not in unique_word:
-            unique_word.append(word)
+def get_count_of_basic_valid_passphrases(passphrases):
+    count_valid_passphrases = 0
+    passphrases_list = passphrases.split('\n')
+    passphrases_list = sanitize(passphrases_list)
+    for passphrase in passphrases_list:
+        if is_basic_passphrase_valid(passphrase):
+            count_valid_passphrases += 1
+    return count_valid_passphrases
+
+
+def get_count_of_valid_advanced_passphrases(passphrases):
+    count_valid_passphrases = 0
+    passphrases_list = passphrases.split('\n')
+    passphrases_list = sanitize(passphrases_list)
+    for passphrase in passphrases_list:
+        if is_advanced_passphrase_valid(passphrase):
+            count_valid_passphrases += 1
+    return count_valid_passphrases
+
+
+def is_basic_passphrase_valid(passphrases):
+    unique_passphrase = []
+    passphrase_list = passphrases.split(' ')
+    for passphrase in passphrase_list:
+        if passphrase not in unique_passphrase:
+            unique_passphrase.append(passphrase)
         else:
             return False
     return True
 
 
-def fetch_count_validation_anagram(words):
-    unique_word = []
-    word_list = words.split(' ')
-    for word in word_list:
-        word_sorted = sorted(word)
-        word_sorted = ''.join(word_sorted)
-        if word_sorted not in unique_word:
-            unique_word.append(word_sorted)
+def is_advanced_passphrase_valid(passphrases): #cambiar passphrase
+    unique_passphrase = []
+    passphrases_list = passphrases.split(' ')
+    for passphrase in passphrases_list:
+        passphrase_sorted = sorted(passphrase)
+        passphrase_sorted = ''.join(passphrase_sorted)
+        if passphrase_sorted not in unique_passphrase:
+            unique_passphrase.append(passphrase_sorted)
         else:
             return False
     return True
 
 
-count = fetch_count_validation('aa bb cc dd')
-valid_anagram = fetch_count_validation_anagram('iiii oiii ooii oooi oooo')
-print(valid_anagram)
+def sanitize(passphrases_list):
+    passphrases_list = [passphrase.strip() for passphrase in passphrases_list]
+    passphrases_list = [passphrase for passphrase in passphrases_list if len(passphrase) > 0]
+    return passphrases_list
--- stylesage_api/passphrase_validation/views.py
+++ stylesage_api/passphrase_validation/views.py
@@ -1,23 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from .services import fetch_count_validation, fetch_count_validation_anagram
+from .services import get_count_of_basic_valid_passphrases, get_count_of_valid_advanced_passphrases
 
 
-def get_passphrase_validation(request):
-    count_valid_passphrases = 0
-    word_list = ['aa bb cc dd', 'aa bb cc dd aa', 'aa bb cc dd aaa', 'uu, uu, aa']
-    for word in word_list:
-        valid_passphrase = fetch_count_validation(word)
-        if valid_passphrase:
-            count_valid_passphrases += 1
+def get_basic_passphrase_validation(request):
+    passphrases = request.body.decode("utf-8")
+    count_valid_passphrases = get_count_of_basic_valid_passphrases(passphrases)
     return HttpResponse(count_valid_passphrases)
 
 
-def get_passphrase_anagram_validation(request):
-    count_valid_passphrases = 0
-    word_list = ['abcde fghij', 'abcde xyz ecdab', 'a ab abc abd abf abj', 'iiii oiii ooii oooi oooo']
-    for word in word_list:
-        valid_passphrase = fetch_count_validation_anagram(word)
-        if valid_passphrase:
-            count_valid_passphrases += 1
+def get_advanced_passphrase_validation(request):
+    passphrases = request.body.decode("utf-8")
+    count_valid_passphrases = get_count_of_valid_advanced_passphrases(passphrases)
     return HttpResponse(count_valid_passphrases)
