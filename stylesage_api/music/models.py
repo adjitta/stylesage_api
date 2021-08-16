b3147fb5240f81a901130bb94a2f7c772ec2f909
adja <adja.kane@cabify.com>
2021-08-16 17:41:38 +0200

    introduce artist_media_url in model and scraping artist image

--- stylesage_api/music/models.py
+++ stylesage_api/music/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Artist(models.Model):
     artist_id = models.IntegerField(primary_key=True)
     artist_name = models.CharField(max_length=150)
+    media_url = models.URLField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.artist_id}: {self.artist_name}'
 
 
 class Album(models.Model):
